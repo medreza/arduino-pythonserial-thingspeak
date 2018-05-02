21d975fb6e458cd18bb411a0bef204e3cadbeca4
Ahmed Reza Rafsanzani <31310263+medreza@users.noreply.github.com>
2018-05-02 15:18:39 +0700

    Add regex, get request, and requirements file

--- readserial-pushthingspeak.py
+++ readserial-pushthingspeak.py
@@ -1,6 +1,19 @@
 import serial
-arduino = serial.Serial('COM5', 115200, timeout=.1)
+import httplib2
+import re
+
+arduino = serial.Serial('COM6', 115200, timeout=1)
 while True:
-	data = arduino.readline()[:-2] #the last bit gets rid of the new-line chars
-	if data:
-		print data
+        data = arduino.readline() #[:-2] the last bit gets rid of the new-line chars
+        if data:
+                print data
+                resp, content = httplib2.Http().request("127.0.0.1")
+                print resp.status
+                matchObj = re.match( r'-(.+):(.+)-', data, re.M|re.I) #-234:654-
+                if matchObj:
+                   print "Read data format : ", matchObj.group()
+                   print "Temperature : ", matchObj.group(1)
+                   print "Humidity : ", matchObj.group(2)
+                else:
+                   print "Unable to read data format."
+
